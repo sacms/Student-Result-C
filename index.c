985c8b07e8d30a42affd5ee1f92b823db37c47ac
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2023-05-27 12:25:53 +0600

    Update index.c

--- index.c
+++ index.c
@@ -21,7 +21,7 @@ WEB : https://shakib.eu.org
      } else if (result >= 1.00) {
         printf("%s is lowest student he got cgpa %2.2f and his roll number is %i \n", name, result, roll);
      } else if (result <= 1.00) {
-        printf("%s isn't regular student he got cgp %2.2f and her roll number is %i \n", name, result, roll);
+        printf("%s isn't regular student he got cgpa %2.2f and her roll number is %i \n", name, result, roll);
      } else {
 
          printf("SERVER DOESN'T EXIST");
@@ -33,16 +33,24 @@ WEB : https://shakib.eu.org
  }
 
  int main() {
-     vaild_std("Afia Sultana", 4746981, 4.00);
-     vaild_std("Robeen H. Mir", 4746986, 4.00);
-     vaild_std("Don X", 4744853, 3.00);
-     vaild_std("DON Y", 474881, 2.00);
-     vaild_std("DON Z", 4746921, 1.00);
+     vaild_std("Afia Sultana", 4746981, 4.00); // return Afia Sultana is the meritorious student he got cgpa 4.00 and his roll number is 4746981 
+
+     vaild_std("Robeen H. Mir", 4746986, 4.00); // return Robeen H. Mir is the meritorious student he got cgpa 4.00 and his roll number is 4746986 
+
+     
+     vaild_std("Don X", 4744853, 3.00); // return Don X is the good student he got cgpa 3.00 and his roll number is 4744853 
+
+     
+     vaild_std("DON Y", 474881, 2.00); // return DON Y is middle student he got cgpa 2.00 and his roll number is 474881 
+
+     
+     vaild_std("DON Z", 4746928, 1.00); // return DON Z is lowest student he got cgpa 1.00 and his roll number is 4746921 
+     
+     vaild_std("DON XYZ", 4746922, 0.76); // return DON XYZ isn't regular student he got cgpa 0.76 and her roll number is 4746922 
      
      
   
 
           return 0;
 }
-
 
